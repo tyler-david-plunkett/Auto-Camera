81d8eb3258f1c9626a147198fcb76cb7dbb9ce3a
tplunke1 <tplunke1@vols.utk.edu>
2020-10-29 23:10:55 -0400

    fixing build paths

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 mkdir -p Auto-Camera/Auto-Camera
-cd Auto-Camera
-cp *.toc Auto-Camera
-cp *.lua Auto-Camera
-cp -r modules Auto-Camera
-cp *.xml Auto-Camera
+cp *.toc Auto-Camera/Auto-Camera
+cp *.lua Auto-Camera/Auto-Camera
+cp -r modules Auto-Camera/Auto-Camera
+cp *.xml Auto-Camera/Auto-Camera
