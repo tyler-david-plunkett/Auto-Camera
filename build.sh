8cca73e4edf8f90fa1aa5c682bff7c977ac21e73
tplunke1 <tplunke1@vols.utk.edu>
2020-11-02 23:55:20 -0500

    adding saved variables adding Ace 3 adding options to Interface Settings UI

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ mkdir -p Auto-Camera/Auto-Camera
 cp *.toc Auto-Camera/Auto-Camera
 cp *.lua Auto-Camera/Auto-Camera
 cp -r modules Auto-Camera/Auto-Camera
+cp -r libs Auto-Camera/Auto-Camera
 cp *.xml Auto-Camera/Auto-Camera
--- main.lua
+++ main.lua
@@ -1,20 +1,73 @@
--- todo define zoom event behavior
-local addonName, addon = ...
-local AutoCamera = addon
+local addonName, vars = ...
+ActionCamera = LibStub("AceAddon-3.0"):NewAddon(addonName)
+local addon = ActionCamera
 local AUTO_CAMERA_ENABLED = true -- todo> make this a setting
 local previousCameraZoom = GetCameraZoom()
-local deltaTime = 0.1 -- deltaTime
+local deltaTime = 0.1
+local defaults = {
+	global = {
+		exitView = 1,
+		humanDistance = 3.5,
+		worgenDistance = 4.6,
+		ridingDistance = 8.5,
+		eliteDistance = 8.5,
+		bossDistance = 50
+	}
+}
+local settings = defaults.global
 local units = {}
-local exitView = 2 -- todo> setting
-
-BINDING_HEADER_AUTO_CAMERA = "Auto-Camera"
-BINDING_NAME_TOGGLE_AUTO_CAMERA = "Toggle On/Off"
-
 units[1] = 'target'
 for i = 1, 10 do
 	units[i + 1] = 'nameplate' .. i
 end
- 
+
+BINDING_HEADER_AUTO_CAMERA = "Auto-Camera"
+BINDING_NAME_TOGGLE_AUTO_CAMERA = "Toggle On/Off"
+
+local function deepCopy(tree)
+	if not tree then return nil
+	elseif type(tree) == "table" then
+		local branch = {}
+		
+		for key, value in pairs(tree) do
+			branch[key] = deepCopy(value)
+		end
+		
+		return branch
+	else
+		return tree
+	end
+end
+
+function addon:RefreshConfig()
+	settings = addon.db.global
+	addon.settings = settings
+	for key, value in pairs(defaults.global) do
+	    if settings[key] == nil then
+			settings[key] = deepCopy(value)
+		end
+	end
+end
+
+-- addon hook callback functions
+function addon:OnInitialize()
+	local options = addon:options()
+	addon.db = LibStub("AceDB-3.0"):New("AutoCameraDB", defaultSettings, true)
+	addon:RefreshConfig()
+	LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options, nil)
+	LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addonName, addonName, nil, "general")
+	options.args.globals = LibStub("AceDBOptions-3.0"):GetOptionsTable(addon.db)
+end
+
+function addon:OnEnable()
+-- Called when the addon is enabled
+end
+
+function addon:OnDisable()
+-- Called when the addon is disabled
+end
+
+-- helper functions
 function toggleAutoCamera()
 	if (AUTO_CAMERA_ENABLED) then
 		AUTO_CAMERA_ENABLED = false
@@ -31,16 +84,16 @@ function autoZoom()
 	local enemyCount = 0
 
 	if (isWorgenForm()) then
-		targetZoom = 4.6
+		targetZoom = settings.worgenDistance
 	else
-		targetZoom = 3.5
+		targetZoom = settings.humanDistance
 	end
 	
 	if (
 		AuraUtil.FindAuraByName("Running Wild", "player") == nil and
 		(IsMounted("player") or UnitInVehicle("player"))
 	) then
-		targetZoom = 8.5
+		targetZoom = settings.ridingDistance
 	end
 
 	local unitCount = 0
@@ -56,13 +109,13 @@ function autoZoom()
 			unitCount = unitCount + 1
 			if (
 				(unitClassification == "worldboss" or
-				(unitClassification == "elite" and UnitLevel(unit) == -1)) and
-				targetZoom < 50
-			) then targetZoom = 50
+				(unitClassification == "elite" and UnitLevel(unit) == -1)) -- and
+				-- targetZoom < 50 -- todo> is this necessary?
+			) then targetZoom = settings.bossDistance
 			elseif (
-				unitClassification == "elite" and
-				targetZoom < 8.5
-			) then targetZoom = 8.5 end
+				unitClassification == "elite" -- and
+				-- targetZoom < 8.5 -- todo> is this necessary
+			) then targetZoom = settings.eliteDistance end
 		end
 	end
 
@@ -93,10 +146,99 @@ function autoZoom()
 	else
 		MoveViewInStop()
 		MoveViewOutStop()
-		SetView(exitView)
+		SetView(settings.exitView)
 	end
 
 	previousCameraZoom = currentCameraZoom
+end
+
+-- option functions
+function addon:GetExitView(info)
+    return settings.exitView
+end
+
+function addon:SetExitView(info, input)
+	print("Exit View = ", input)
+    settings.exitView = input
+end
+
+function merge(target, source)
+	for key, value in pairs(source) do target[key] = value end
+	return target
+end
+
+function distanceOption()
+	return {
+		type = 'range',
+		min = 0,
+		max = 50,
+		step = 0.1
+	}
+end
+
+-- options
+function addon:options()
+	return {
+		type = 'group',
+		set = function(info, val) settings[info[#info]] = val end,
+		get = function(info) return settings[info[#info]] end,
+		args = {
+			general = {
+				type = "group",
+				name = "General",
+				args = {
+					standingDistances = {
+						type = "group",
+						inline = true,
+						order = 1,
+						name = "Standing Distances by Race",
+						args = {
+							humanDistance = merge(distanceOption(), {
+								name = 'Human'
+							}),
+							worgenDistance = merge(distanceOption(), {
+								name = 'Worgen'
+							}),
+						}
+					},
+					contexturalDistances = {
+						type = "group",
+						inline = true,
+						order = 2,
+						name = "Contextual Distances",
+						args = {
+							ridingDistance = merge(distanceOption(), {
+								name = 'Riding',
+								desc = 'On mount or in a vehicle'
+							}),
+							eliteDistance = merge(distanceOption(), {
+								name = 'Elite Combat'
+							}),
+							bossDistance = merge(distanceOption(), {
+								name = 'Boss Combat'
+							}),
+						}
+					},
+					misc = {
+						type = "group",
+						inline = true,
+						order = 3,
+						name = "Miscellaneous",
+						args = {
+							exitView = {
+								type = 'range',
+								min = 1,
+								max = 5,
+								step = 1,
+								name = 'Exit View',
+								desc = 'The camera view to go to when toggling Auto-Camera off'
+							}
+						}
+					}
+				}
+			}
+		},
+	}
 end
 
 -- commands
