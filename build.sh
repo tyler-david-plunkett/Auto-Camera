7672790950fe276eb41861f0ba8749f16f02cb05
tplunke1 <tplunke1@vols.utk.edu>
2020-11-08 21:03:51 -0500

    updating version updating build script

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-mkdir -p Auto-Camera/Auto-Camera
-cp *.toc Auto-Camera/Auto-Camera
-cp *.lua Auto-Camera/Auto-Camera
-cp -r modules Auto-Camera/Auto-Camera
-cp -r libs Auto-Camera/Auto-Camera
-cp *.xml Auto-Camera/Auto-Camera
+mkdir Auto-Camera
+cp *.toc Auto-Camera
+cp *.lua Auto-Camera
+cp -r modules Auto-Camera
+cp -r libs Auto-Camera
+cp *.xml Auto-Camera
+zip -r Auto-Camera.zip Auto-Camera
+rm -r Auto-Camera
