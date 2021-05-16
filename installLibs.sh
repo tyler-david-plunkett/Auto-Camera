e8522e2d06bd57a88984040c5de0023fe2ef80d5
tyler-david-plunkett <57200004+tyler-david-plunkett@users.noreply.github.com>
2021-05-16 19:15:40 -0400

    fix install lib script

--- installLibs.sh
+++ installLibs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir libs
-wget -O Ace.zip https://media.forgecdn.net/files/3078/383/Ace3-Release-r1241.zip
-unzip Ace.zip
-cp -r Ace/AceAddon-3.0 libs
-cp -r Ace/AceConfig-3.0 libs
-cp -r Ace/AceDB-3.0 libs
-cp -r Ace/AceDBOptions-3.0 libs
-cp -r Ace/AceGUI-3.0 libs
-cp -r Ace/CallbackHandler-1.0 libs
-cp -r Ace/LibStub libs
+wget -O Ace3.zip https://media.forgecdn.net/files/3078/383/Ace3-Release-r1241.zip
+unzip Ace3.zip
+cp -r Ace3/AceAddon-3.0 libs
+cp -r Ace3/AceConfig-3.0 libs
+cp -r Ace3/AceDB-3.0 libs
+cp -r Ace3/AceDBOptions-3.0 libs
+cp -r Ace3/AceGUI-3.0 libs
+cp -r Ace3/CallbackHandler-1.0 libs
+cp -r Ace3/LibStub libs
