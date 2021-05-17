7eb91e0da6bdedf711870950a721372ea2919c38
tyler-david-plunkett <57200004+tyler-david-plunkett@users.noreply.github.com>
2021-05-16 22:51:15 -0400

    removing build copies to be handled differently

--- build.sh
+++ build.sh
@@ -7,6 +7,4 @@ cp -r modules Auto-Camera
 cp -r libs Auto-Camera
 cp *.xml Auto-Camera
 zip -r Auto-Camera.zip Auto-Camera
-cp Auto-Camera.zip Auto-Camera-bc.zip
-cp Auto-Camera.zip Auto-Camera-classic.zip
 rm -r Auto-Camera
