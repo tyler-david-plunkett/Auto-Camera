b845e865e71c840f964c714301ae293b007ef2c3
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2022-05-07 18:24:56 -0400

    fix toc file names

--- build.sh
+++ build.sh
@@ -7,10 +7,8 @@ cp -r libs Auto-Camera
 cp *.xml Auto-Camera
 cp Auto-Camera.toc Auto-Camera
 zip -r Auto-Camera.zip Auto-Camera
-rm Auto-Camera/Auto-Camera.toc
-cp Auto-Camera-Classic.toc Auto-Camera
+cp Auto-Camera-Classic.toc Auto-Camera/Auto-Camera.toc
 zip -r Auto-Camera-classic.zip Auto-Camera
-rm Auto-Camera/Auto-Camera-classic.toc
-cp Auto-Camera-BCC.toc Auto-Camera
+cp Auto-Camera-BCC.toc Auto-Camera/Auto-Camera.toc
 zip -r Auto-Camera-bcc.zip Auto-Camera
 rm -r Auto-Camera
