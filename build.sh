369f967adaff2f88d5b42e008198318e673375ed
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2022-05-07 10:51:40 -0400

    add classic and bcc to build script

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 mkdir Auto-Camera
-cp *.toc Auto-Camera
 cp *.lua Auto-Camera
 cp -r modules Auto-Camera
 cp -r libs Auto-Camera
 cp *.xml Auto-Camera
+cp Auto-Camera.toc Auto-Camera
 zip -r Auto-Camera.zip Auto-Camera
+rm Auto-Camera/Auto-Camera.toc
+cp Auto-Camera-Classic.toc Auto-Camera
+zip -r Auto-Camera-classic.zip Auto-Camera
+rm Auto-Camera/Auto-Camera-classic.toc
+cp Auto-Camera-BCC.toc Auto-Camera
+zip -r Auto-Camera-bcc.zip Auto-Camera
 rm -r Auto-Camera
