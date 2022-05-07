43162d7bc7871dbbfea04f3133ae2a7e84ebd2fa
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2022-05-07 19:33:17 -0400

    add release.json generation to build script

--- build.sh
+++ build.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+# scan for versions
+addonVersion=$1
+retailInterfaceVersion=`grep "## Interface" Auto-Camera.toc | grep -oEi "[0-9]*"`
+classicInterfaceVersion=`grep "## Interface" Auto-Camera-Classic.toc | grep -oEi "[0-9]*"`
+bccInterfaceVersion=`grep "## Interface" Auto-Camera-BCC.toc | grep -oEi "[0-9]*"`
+
 mkdir Auto-Camera
 cp *.lua Auto-Camera
 cp -r modules Auto-Camera
 cp -r libs Auto-Camera
 cp *.xml Auto-Camera
+
+# create retail artifact
 cp Auto-Camera.toc Auto-Camera
 zip -r Auto-Camera.zip Auto-Camera
+
+# create classic artifact
 cp Auto-Camera-Classic.toc Auto-Camera/Auto-Camera.toc
 zip -r Auto-Camera-classic.zip Auto-Camera
+
+# create bcc artifact
 cp Auto-Camera-BCC.toc Auto-Camera/Auto-Camera.toc
 zip -r Auto-Camera-bcc.zip Auto-Camera
+
+# create release.json
+releaseJSON='{"releases":[{"filename":"%s.zip","metadata":[{"flavor":"mainline","interface":%s}]},{"filename":"%s-classic.zip","metadata":[{"flavor":"classic","interface":%s}]},{"filename":"%s-bcc.zip","metadata":[{"flavor":"bcc","interface":%s}]}]}'
+printf "$releaseJSON" "$addonVersion" "$retailInterfaceVersion" "$addonVersion" "$classicInterfaceVersion" "$addonVersion" "$bccInterfaceVersion" > release.json
+
+# clean up
 rm -r Auto-Camera
