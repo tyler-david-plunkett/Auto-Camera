5db7a83576312865dd240d2951a0591bfe1e6936
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2022-05-07 21:16:59 -0400

    update lib install to use latest ace3 release

--- installLibs.sh
+++ installLibs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkdir libs
-wget -O Ace3.zip https://media.forgecdn.net/files/3078/383/Ace3-Release-r1241.zip
+curl -L https://www.wowace.com/projects/ace3/files/latest -o Ace3.zip
+
 unzip Ace3.zip
 cp -r Ace3/AceAddon-3.0 libs
 cp -r Ace3/AceConfig-3.0 libs
@@ -10,3 +11,6 @@ cp -r Ace3/AceDBOptions-3.0 libs
 cp -r Ace3/AceGUI-3.0 libs
 cp -r Ace3/CallbackHandler-1.0 libs
 cp -r Ace3/LibStub libs
+
+rm -r Ace3
+rm Ace3.zip
