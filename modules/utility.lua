a3113840c3171d8e9e1685772b6689387c973b1f
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2022-08-28 17:31:32 -0400

    whitespace refactor

--- modules/utility.lua
+++ modules/utility.lua
@@ -13,13 +13,13 @@ function deepCopy(tree)
     end
 end
 
-function assign (target, source)
+function assign(target, source)
     table.foreach(source, function(key, value)
         target[key] = value
     end)
 end
 
-function set (list)
+function set(list)
     local set = {}
     for _, l in ipairs(list) do set[l] = true end
     return set
