e774f988d25d400f1e0e5aa1b00bcc921257c466
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2022-11-15 18:57:19 -0500

    update build script release file output

--- build.sh
+++ build.sh
@@ -30,8 +30,8 @@ cp Auto-Camera-Wrath.toc Auto-Camera/Auto-Camera.toc
 zip -r Auto-Camera-wrath.zip Auto-Camera
 
 # create release.json
-releaseJSON='{"releases":[{"filename":"Auto-Camera-%s.zip","metadata":[{"flavor":"mainline","interface":%s}]},{"filename":"Auto-Camera-%s-classic.zip","metadata":[{"flavor":"classic","interface":%s}]},{"filename":"Auto-Camera-%s-bcc.zip","metadata":[{"flavor":"bcc","interface":%s}]},{"filename":"Auto-Camera-%s-wrath.zip","metadata":[{"flavor":"wrath","interface":%s}]}]}'
-printf "$releaseJSON" "$addonVersion" "$retailInterfaceVersion" "$addonVersion" "$classicInterfaceVersion" "$addonVersion" "$bccInterfaceVersion" "$addonVersion" "$wrathInterfaceVersion" > release.json
+releaseJSON='{"releases":[{"name":"Auto-Camera","version":"%s","filename":"Auto-Camera-%s.zip","nolib": false,"metadata":[{"flavor":"mainline","interface":%s}]},{"name":"Auto-Camera","version":"%s","filename":"Auto-Camera-%s-classic.zip","nolib": false,"metadata":[{"flavor":"classic","interface":%s}]},{"name":"Auto-Camera","version":"%s","filename":"Auto-Camera-%s-bcc.zip","nolib": false,"metadata":[{"flavor":"bcc","interface":%s}]},{"name":"Auto-Camera","version":"%s","filename":"Auto-Camera-%s-wrath.zip","nolib": false,"metadata":[{"flavor":"wrath","interface":%s}]}]}'
+printf "$releaseJSON" "$addonVersion" "$addonVersion" "$retailInterfaceVersion" "$addonVersion" "$addonVersion" "$classicInterfaceVersion" "$addonVersion" "$addonVersion" "$bccInterfaceVersion" "$addonVersion" "$addonVersion" "$wrathInterfaceVersion" > release.json
 
 # clean up
 rm -r Auto-Camera
