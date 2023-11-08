012a37f8f5c91a815f402e31f91515e47eda5a59
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2023-11-07 21:54:09 -0500

    fix use of removed function

--- modules/actionCamCVars.lua
+++ modules/actionCamCVars.lua
@@ -1,6 +1,6 @@
 local addonName, T = ...
 T.actionCamCVars = {} -- todo: non global assignment?
-for key, command in pairs(C_Console.GetAllCommands()) do
+for key, command in pairs(ConsoleGetAllCommands()) do
   if (command.commandType == 0 and strfind(command.command, 'test_camera') ~= nil) then
       table.insert(T.actionCamCVars, command.command)
   end
