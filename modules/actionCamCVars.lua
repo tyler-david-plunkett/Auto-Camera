93cf34d278f1aeadc557e35aee6e125c928e9bce
tyler-david-plunkett <57200004+pocket-person@users.noreply.github.com>
2023-11-08 17:03:43 -0500

    fix undefined command and bump toc files

--- modules/actionCamCVars.lua
+++ modules/actionCamCVars.lua
@@ -1,6 +1,6 @@
 local addonName, T = ...
 T.actionCamCVars = {} -- todo: non global assignment?
-for key, command in pairs(ConsoleGetAllCommands()) do
+for key, command in pairs((ConsoleGetAllCommands and ConsoleGetAllCommands()) or (C_Console and C_Console.GetAllCommands())) do
   if (command.commandType == 0 and strfind(command.command, 'test_camera') ~= nil) then
       table.insert(T.actionCamCVars, command.command)
   end
